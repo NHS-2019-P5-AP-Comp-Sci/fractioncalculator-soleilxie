3bfa52b986f223019478a92e291ec572919fe9c2
s-xies <s-xies@53S500036862.it.bsd405.org>
2019-11-15 22:39:32 -0800

    Checkpoint 1.

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,13 +3,24 @@
  */
 
 package fracCalc;
+import java.util.Scanner;
 
 public class FracCalc {
 
     public static void main(String[] args)
     {
+    	
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	Scanner userInput = new Scanner(System.in);
+    	String frac = " + ";
+    	while(!frac.equals("quit"))
+		{
+			
+			System.out.println(produceAnswer(frac));
+	    	System.out.println("Enter the equation (to stop type 'quit'): ");
+	    	frac = userInput.nextLine();
+		}
+    	
     }
 
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -23,8 +34,46 @@ public class FracCalc {
     public static String produceAnswer(String input)
     {
         // TODO: Implement this function to produce the solution to the input
-
-        return "";
+    	int spaceIndex1 = input.indexOf(" ");
+    	String firstValue = input.substring(0, spaceIndex1);
+    	String operand = input.substring(spaceIndex1 + 1, spaceIndex1 + 2);
+    	String secondValue = input.substring(spaceIndex1 + 3);
+    	int wholeIdx = 0;
+    	String whole = "0";
+    	String numerator = "0";
+    	String denominator = "1";
+    	
+    	int i = 0;
+    	int slashIdx = 0;
+    	while(i < secondValue.length())
+    	{
+    		if(secondValue.substring(i, i+1).equals("/"))
+    		{
+    			slashIdx = i;
+    		}
+    		if(secondValue.substring(i, i+1).equals("_"))
+    		{
+    	    	wholeIdx = i;
+    		}
+    		i++;
+    	}
+    	
+    	if(wholeIdx !=0)
+    	{
+    		whole = secondValue.substring(0, wholeIdx);
+    		wholeIdx++;
+    	}
+    	if(slashIdx !=0)
+    	{
+    		
+    		numerator = secondValue.substring(wholeIdx, slashIdx);
+    		denominator = secondValue.substring(slashIdx+ 1);
+    	} else {
+    		numerator = secondValue;
+    	}
+    	String retVal = "whole:"+ whole + " numerator:" + numerator + " denominator:" + denominator;
+    	retVal = secondValue;
+        return retVal;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
